6109a1b79cee40859462247485020b049812639d
Eugenio <eugenio_unito>
2025-06-18 17:31:29 +0200

    fix update to node 16.20.2

---
+++ src/pages/resultsView/structuralVariant/chromoscope/default-encoding.ts
@@ -0,0 +1,21 @@
+export default {
+    color: {
+        svclass: {
+            // definitions about head-to-head and tail-to-tail inversion,
+            // see https://genomebiology.biomedcentral.com/articles/10.1186/s13059-018-1421-5/figures/7
+            domain: [
+                'Translocation',
+                'Duplication',
+                'Deletion',
+                'Inversion (TtT)',
+                'Inversion (HtH)',
+            ],
+            range: ['lightgrey', '#409F7A', '#3275B4', '#CC7DAA', '#E6A01B'],
+            Translocation: 'lightgrey',
+            Duplication: '#409F7A',
+            Deletion: '#3275B4',
+            'Inversion (TtT)': '#CC7DAA',
+            'Inversion (HtH)': '#E6A01B',
+        },
+    },
+};
---
+++ src/pages/resultsView/structuralVariant/chromoscope/index.ts
@@ -0,0 +1,9 @@
+import sv from './sv';
+import boundary from './boundary';
+
+export type TrackMode = 'small' | 'top' | 'mid';
+
+export default {
+    sv,
+    boundary,
+};
